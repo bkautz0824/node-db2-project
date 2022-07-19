302112f86ccadefb2dd74906c93292c6bf684197
Bennett Kautz <bennettkautz@gmail.com>
2022-07-19 16:55:18 -0400

    router/model/middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,9 +1,34 @@
-const checkCarId = (req, res, next) => {
+const Car = require('./cars-model')
+
+
+const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    const car = await Car.getById(req.params.id)
+    if(!car){
+      res.status(404).json({
+        message: `car with id ${req.params.id} is not found`
+      })
+    } else{
+      req.car = car
+      next()
+    }
+}catch(err){
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if(!req.body.vin) return next({ status: 400, message: `vin is missing`})
+
+  if(!req.body.make) return next({ status: 400, message: `make is missing`})
+
+  if(!req.body.model) return next({ status: 400, message: `model is missing`})
+
+  if(!req.body.mileage) return next({ status: 400, message: `mileage is missing`})
+
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
